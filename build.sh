77a3db7a505e116ca3b9eb6b59439fdb9fac7996
kritoke <3081486+kritoke@users.noreply.github.com>
2026-09-19 15:09:23 -0500

    Update build script to shrink file size, new binaries.

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
-GOOS=windows GOARCH=amd64 go build -o bin/iplookup-amd64.exe
-GOOS=darwin GOARCH=arm64 go build -o bin/iplookup-arm64-darwin
-GOOS=linux GOARCH=amd64 go build -o bin/iplookup-amd64-linux
+GOOS=windows GOARCH=amd64 go build -ldflags="-s -w" -o bin/iplookup-amd64.exe
+GOOS=darwin GOARCH=arm64 go build -ldflags="-s -w" -o bin/iplookup-arm64-darwin
+GOOS=linux GOARCH=amd64 go build -ldflags="-s -w" -o bin/iplookup-amd64-linux
